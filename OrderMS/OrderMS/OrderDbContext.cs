5c0182cba05b61a5f7e5e7922e07842c96a2a608
Kelly E. Osoba <89794417+Osoba001@users.noreply.github.com>
2023-05-31 03:20:33 +0100

    Setup order dbcontext

---
+++ OrderMS/OrderMS/OrderDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace OrderMS
+{
+    public class OrderDbContext:DbContext
+    {
+        public OrderDbContext(DbContextOptions<OrderDbContext> options):base(options)
+        {
+        
+        }
+
+        public DbSet<Order> OrderTb { get; set; }
+    }
+}
