0a2bc232930d71246b36954648ab30f4702076d6
Kelly E. Osoba <89794417+Osoba001@users.noreply.github.com>
2023-05-31 03:21:59 +0100

    Web api project for making order

---
+++ OrderMS/OrderMS/Order.cs
@@ -0,0 +1,10 @@
+﻿namespace OrderMS
+{
+    public class Order
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int ProductId { get; set; }
+        public int Quantiy { get; set; }
+    }
+}
