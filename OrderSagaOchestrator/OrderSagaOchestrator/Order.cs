3d764a680ce71d7f15c838258b38d2b7defa28d2
Kelly E. Osoba <89794417+Osoba001@users.noreply.github.com>
2023-06-01 07:42:54 +0100

    Added na Order model request

---
+++ OrderSagaOchestrator/OrderSagaOchestrator/Order.cs
@@ -0,0 +1,10 @@
+﻿namespace OrderSagaOchestrator
+{
+    public class Order
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int ProductId { get; set; }
+        public int Quantiy { get; set; }
+    }
+}
