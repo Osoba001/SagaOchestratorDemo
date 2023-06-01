501ae114171733d6de9ec9cf3bfa0b4b9675ba4e
Kelly E. Osoba <89794417+Osoba001@users.noreply.github.com>
2023-06-01 07:40:37 +0100

    Added AbstractRabbitMQ dependency for publisher

--- OrderSagaOchestrator/OrderSagaOchestrator/Program.cs
+++ OrderSagaOchestrator/OrderSagaOchestrator/Program.cs
@@ -1,8 +1,23 @@
+using AbstractedRabbitMQ.Setup;
+using OrderSagaOchestrator.Services;
+using RabbitMQ.Client;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
+
+builder.Services.AddRabbitMQConnection(x =>
+{
+    x.Url = "amqp://guest:guest@localhost:5672";
+    x.ClientProvideName = "SagaOchestriator App";
+}).AddRabbitMQPublisher(x =>
+{
+    x.exchange = "order-created";
+    x.exchangeType = ExchangeType.Direct;
+});
+builder.Services.AddHttpClient<ICreateOrderService, CreateOrderService>();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
