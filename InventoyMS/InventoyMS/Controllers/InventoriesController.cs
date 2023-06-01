6cb192a4a5a7100a3c637041f2f75a59369b14c8
Kelly E. Osoba <89794417+Osoba001@users.noreply.github.com>
2023-06-01 07:35:35 +0100

    Scaffolded Inventory controller with entity framework

---
+++ InventoyMS/InventoyMS/Controllers/InventoriesController.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using InventoyMS;
+
+namespace InventoyMS.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class InventoriesController : ControllerBase
+    {
+        private readonly InventoryDbContext _context;
+
+        public InventoriesController(InventoryDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Inventories
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Inventory>>> GetInventoryTb()
+        {
+          if (_context.InventoryTb == null)
+          {
+              return NotFound();
+          }
+            return await _context.InventoryTb.ToListAsync();
+        }
+
+        // GET: api/Inventories/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Inventory>> GetInventory(int id)
+        {
+          if (_context.InventoryTb == null)
+          {
+              return NotFound();
+          }
+            var inventory = await _context.InventoryTb.FindAsync(id);
+
+            if (inventory == null)
+            {
+                return NotFound();
+            }
+
+            return inventory;
+        }
+
+        // PUT: api/Inventories/5
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutInventory(int id, Inventory inventory)
+        {
+            if (id != inventory.Id)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(inventory).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!InventoryExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/Inventories
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPost]
+        public async Task<ActionResult<Inventory>> PostInventory(Inventory inventory)
+        {
+          if (_context.InventoryTb == null)
+          {
+              return Problem("Entity set 'InventoryDbContext.InventoryTb'  is null.");
+          }
+            _context.InventoryTb.Add(inventory);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetInventory", new { id = inventory.Id }, inventory);
+        }
+
+        // DELETE: api/Inventories/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteInventory(int id)
+        {
+            if (_context.InventoryTb == null)
+            {
+                return NotFound();
+            }
+            var inventory = await _context.InventoryTb.FindAsync(id);
+            if (inventory == null)
+            {
+                return NotFound();
+            }
+
+            _context.InventoryTb.Remove(inventory);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool InventoryExists(int id)
+        {
+            return (_context.InventoryTb?.Any(e => e.Id == id)).GetValueOrDefault();
+        }
+    }
+}
